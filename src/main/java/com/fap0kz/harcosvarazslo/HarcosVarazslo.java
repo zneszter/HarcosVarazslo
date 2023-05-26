dc23346bc15d6c47d26aed0aff9df4266bca8d2e
Zsolt Neszter <zneszter@gmail.com>
2023-05-26 07:48:08 +0200

    initial state

---
+++ src/main/java/com/fap0kz/harcosvarazslo/HarcosVarazslo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fap0kz.harcosvarazslo;
+
+/**
+ *
+ * @author neszterzsolt
+ */
+public class HarcosVarazslo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
