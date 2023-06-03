0327cca9356da2b5f464f4825509432ad607131a
Zsolt Neszter <zneszter@gmail.com>
2023-06-03 13:23:51 +0200

    Varazslo class ami kiterjeszti a Karakterek java class-t

---
+++ src/main/java/com/fap0kz/harcosvarazslo/modell/Varazslo.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fap0kz.harcosvarazslo.modell;
+
+/**
+ *
+ * @author neszterzsolt
+ */
+public class Varazslo extends Karakterek{
+    
+    private String nev;
+//    private final String VARAZSLO = "V";
+
+    public Varazslo() {
+        super();
+        this.nev = "V";
+    
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+    
+    
+    
+}
