16d298182d799e6b0d75083963fe2f6a133475c1
Zsolt Neszter <zneszter@gmail.com>
2023-06-03 13:23:11 +0200

    Karakterek java class tartalmazza a tulajdonsagokat

---
+++ src/main/java/com/fap0kz/harcosvarazslo/modell/Karakterek.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fap0kz.harcosvarazslo.modell;
+
+/**
+ *
+ * @author neszterzsolt
+ */
+public class Karakterek {
+    private final int eletero;
+    private String nev;
+//    private final String HARCOS = "H";
+//    private final String VARAZSLO = "V";
+
+    public Karakterek() {
+        this.eletero = Funkciok.eletero();
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public void harc(Karakterek karakterek) {
+    
+    }
+    
+    
+}
